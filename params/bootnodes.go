ee6c1298d9c722b02be672f2cf33d59a742e9818
saeid maroufi <saeidmaroufi66@gmail.com>
2017-10-15 10:10:06 +0330

    change testnet bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -25,7 +25,7 @@ var MainnetBootnodes = []string{
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Ropsten test network.
 var TestnetBootnodes = []string{
-     	"enode://6ce05930c72abc632c58e2e4324f7c7ea478cec0ed4fa2528982cf34483094e9cbc9216e7aa349691242576d552a2a56aaeae426c5303ded677ce455ba1acd9d@94.177.12.112:41207", 
+     	"enode://1bc9ee0115e9a8194c3923cd49d05825a93f91a5467c3e2481dfa4f64516eaf41bde671195d915439c6ec27a034e44bbb311f47a41d9f275adc4f1b9e1274156@94.177.12.112:41207", 
 	}
 
 // RinkebyBootnodes are the enode URLs of the P2P bootstrap nodes running on the
